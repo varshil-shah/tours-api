bac0416e2b1cb169d8e7b97a7e85ca139690a7bd
Varshil-Shah <varshilashah09@gmail.com>
2022-03-09 22:33:49 +0530

    :sparkles: Reade and write multiple files async

--- Introduction/index.js
+++ Introduction/index.js
@@ -1,7 +1,23 @@
 const fs = require('fs');
 
+// BLOCKING CODE - SYNCHRONOUS CODE
 const textIn = fs.readFileSync('txt/input.txt', 'utf-8');
 console.log(textIn);
-
 const textOut = `This messages is added by NodeJS.\nCreated on ${Date.now()}`;
 fs.writeFileSync('txt/output.txt', textOut);
+console.log('File written!');
+
+// NON-BLOCKING CODE - ASYNCHRONOUS CODE
+fs.readFile('txt/start.txt', 'utf-8', (_, data1) => {
+  fs.readFile(`txt/${data1}.txt`, 'utf-8', (_, data2) => {
+    console.log(data2);
+    fs.readFile('txt/append.txt', 'utf-8', (_, data3) => {
+      console.log(data3);
+
+      fs.writeFile('txt/final.txt', `${data2}\n${data3}`, 'utf-8', (_) => {
+        console.log('You file has been written!');
+      });
+    });
+  });
+});
+console.log('Will read file?');
