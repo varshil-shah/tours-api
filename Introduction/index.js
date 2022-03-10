f068b50f043edbbb8f21f38e8b217e2777d44a3c
Varshil-Shah <varshilashah09@gmail.com>
2022-03-10 13:27:30 +0530

    :sparkles: Build dynamic template-overview page

--- Introduction/index.js
+++ Introduction/index.js
@@ -30,14 +30,65 @@ fs.readFile('txt/start.txt', 'utf-8', (_, data1) => {
 console.log('Will read file?');
 */
 
+const templateOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const templateCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const templateProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+const replaceTemplate = (templateCard, product) => {
+  let output = templateCard.replace(/{%NAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%ID%}/g, product.id);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+
+  if (!product.organic)
+    output = output.replace(/{%NON_ORGANIC%}/g, 'not-organic');
+  return output;
+};
+
 // HTTP -
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
-  if (pathName === '/overview') {
-    res.end('<h1>This is an overview page :)</h1>');
+  // Overview page -
+  if (pathName === '/overview' || pathName === '/') {
+    res.writeHead(200, {
+      'Content-type': 'text/html',
+    });
+
+    const cardsHtml = dataObj
+      .map((card) => replaceTemplate(templateCard, card))
+      .join('');
+    const result = templateOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    res.end(result);
+
+    // Product page -
   } else if (pathName === '/product') {
     res.end('<h1>This is an product page :)</h1>');
+
+    // API -
+  } else if (pathName === '/api') {
+    res.writeHead(200, {
+      'Content-type': 'application/json',
+    });
+    res.end(data);
+
+    // Page not found -
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
