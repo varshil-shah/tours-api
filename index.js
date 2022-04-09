8836cb01ec8f0f49326801799ec2247b61e1214c
Varshil-Shah <varshilashah09@gmail.com>
2022-04-09 09:31:11 +0530

    :sparkles: Learn to consume promise

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
+// fs.readFile(`${__dirname}/dog.txt`, 'utf-8', (err, data) => {
+//   superagent
+//     .get(
+//       `https://dog.ceo/api/breed/${data.substring(
+//         0,
+//         data.length - 2
+//       )}/images/random`
+//     )
+//     .end((err, res) => {
+//       if (err) return console.log(err.message);
+//       fs.writeFile('dog-url.txt', res.body.message, (err) => {
+//         console.log(`File saved!`);
+//       });
+//     });
+// });
+
 fs.readFile(`${__dirname}/dog.txt`, 'utf-8', (err, data) => {
   superagent
     .get(
@@ -9,10 +25,16 @@ fs.readFile(`${__dirname}/dog.txt`, 'utf-8', (err, data) => {
         data.length - 2
       )}/images/random`
     )
-    .end((err, res) => {
-      if (err) return console.log(err.message);
+    .then((res) => {
       fs.writeFile('dog-url.txt', res.body.message, (err) => {
+        if (err)
+          return console.log(
+            `Error occured while writing file - ${err.message}`
+          );
         console.log(`File saved!`);
       });
+    })
+    .catch((err) => {
+      console.log(err.message);
     });
 });
