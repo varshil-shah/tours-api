aec93d574b1baf6372f5d41561a1bde1d4ba452e
Varshil-Shah <varshilashah09@gmail.com>
2022-04-09 11:21:26 +0530

    :sparkles: Learn to build promises

--- index.js
+++ index.js
@@ -17,24 +17,58 @@ const superagent = require('superagent');
 //     });
 // });
 
-fs.readFile(`${__dirname}/dog.txt`, 'utf-8', (err, data) => {
-  superagent
-    .get(
-      `https://dog.ceo/api/breed/${data.substring(
-        0,
-        data.length - 2
-      )}/images/random`
-    )
-    .then((res) => {
-      fs.writeFile('dog-url.txt', res.body.message, (err) => {
-        if (err)
-          return console.log(
-            `Error occured while writing file - ${err.message}`
-          );
-        console.log(`File saved!`);
-      });
-    })
-    .catch((err) => {
-      console.log(err.message);
+// CONSUMING PROMISE -
+// fs.readFile(`${__dirname}/dog.txt`, 'utf-8', (err, data) => {
+//   superagent
+//     .get(
+//       `https://dog.ceo/api/breed/${data.substring(
+//         0,
+//         data.length - 2
+//       )}/images/random`
+//     )
+//     .then((res) => {
+//       fs.writeFile('dog-url.txt', res.body.message, (err) => {
+//         if (err)
+//           return console.log(
+//             `Error occured while writing file - ${err.message}`
+//           );
+//         console.log(`File saved!`);
+//       });
+//     })
+//     .catch((err) => {
+//       console.log(err.message);
+//     });
+// });
+
+// READ FILE AND RETURN PROMISE -
+const readFilePromise = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject('I could not find that file 😢');
+      resolve(data);
     });
-});
+  });
+};
+
+const writeFilePromise = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject('Could not write file 😢');
+      resolve('success');
+    });
+  });
+};
+
+readFilePromise(`${__dirname}/dogg.txt`)
+  .then((data) => {
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then((res) => {
+    return writeFilePromise('dog-url.txt', res.body.message);
+  })
+  .then(() => {
+    console.log(`Writing to file completed`);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
