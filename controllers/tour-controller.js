9ca7bc59e5f97337fa04b1f3ef0adb1c9e39e26e
Varshil-Shah <varshilashah09@gmail.com>
2022-04-11 00:03:53 +0530

    :sparkles: Chaining multiple middlewares

--- controllers/tour-controller.js
+++ controllers/tour-controller.js
@@ -4,12 +4,22 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
-exports.checkId = (_, res, next, value) => {
+exports.checkId = (req, res, next, value) => {
   console.log(`Tour id: ${value}`);
   if (+value > tours.length - 1) {
     return res.status(404).json({
       status: 'fail',
       message: 'Id not found!',
+    });
+  }
+  next();
+};
+
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Body must contain name and price property',
     });
   }
   next();
