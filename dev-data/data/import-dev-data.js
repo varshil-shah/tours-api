209742d54ccbe4f73eb0c5f376fba20b48b3a17a
Varshil-Shah <varshilashah09@gmail.com>
2022-04-16 13:39:53 +0530

    :sparkles: Implement func to read/write data
    Implement function to read data from file and write to database and delete all data from database

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const fs = require('fs');
+const dotevn = require('dotenv');
+
+const Tour = require('../../models/tourModel');
+
+dotevn.config({
+  path: './config.env',
+});
+
+const database = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(database, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log(`Database connected successfully!`));
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+// IMPORT DATA AND STORE IN DATABASE -
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log(`Data successfully loaded!`);
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
+
+// DELETE DATA FROM DATABASE -
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log(`All data deleted successfully!`);
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
