fa034e2d784f4c1d747b73c47cce3b3746f6dd4d
Varshil-Shah <varshilashah09@gmail.com>
2022-04-19 17:48:37 +0530

    :sparkles: Implement func to handle outside errors

--- server.js
+++ server.js
@@ -22,6 +22,14 @@ mongoose
 const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
+});
+
+process.on('unhandledRejection', (error) => {
+  console.log(`ERRROR: ${error.name} | ERRROR MSG: ${error.message}`);
+  console.log(`UNHANDLED REJECTION | SHUTTING DOWN ...`);
+  server.close(() => {
+    process.exit(1);
+  });
 });
