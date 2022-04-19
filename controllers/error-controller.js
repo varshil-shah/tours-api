60c1e3b99cad32ec22ee05dec96914ac119b3888
Varshil-Shah <varshilashah09@gmail.com>
2022-04-19 14:28:40 +0530

    :sparkles: Sending error according to NODE_ENV

--- controllers/error-controller.js
+++ controllers/error-controller.js
@@ -1,10 +1,41 @@
+const sendErrorDevelopment = (error, res) => {
+  res.status(error.statusCode).json({
+    status: error.status,
+    message: error.message,
+    stack: error.stack,
+    error: error,
+  });
+};
+
+const sendErrorProduction = (error, res) => {
+  // OPERATIONAL, TRUSTED ERRROR - SEND MESSAGE TO CLIENT
+  if (error.isOperational) {
+    res.status(error.statusCode).json({
+      status: error.status,
+      message: error.message,
+    });
+
+    // PROGRAMMING ERRROR OR OTHER UNKNOWN ERRROR - DON'T LEAK
+  } else {
+    // eslint-disable-next-line no-console
+    console.error(`ERRROR 💣 ${error}`);
+
+    // SEND GENERIC RESPONSE
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong :(',
+    });
+  }
+};
+
 module.exports = (error, req, res, next) => {
   error.statusCode = error.statusCode || 500;
   error.status = error.status || 'error';
 
-  res.status(error.statusCode).json({
-    status: error.status,
-    message: error.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDevelopment(error, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProduction(error, res);
+  }
   next();
 };
