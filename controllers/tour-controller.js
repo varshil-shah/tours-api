31d8aeacf9ecf77c5a426904abf8c5716362c55d
Varshil-Shah <varshilashah09@gmail.com>
2022-04-19 13:18:33 +0530

    :sparkles: Handling 404 errors

--- controllers/tour-controller.js
+++ controllers/tour-controller.js
@@ -1,6 +1,7 @@
 const Tour = require('../models/tourModel');
 const APIFeatures = require('../utils/api-features');
 const catchAsync = require('../utils/catch-async');
+const AppError = require('../utils/app-error');
 
 exports.aliasTopTour = (req, res, next) => {
   req.query.limit = '5';
@@ -26,8 +27,12 @@ exports.getAllTours = catchAsync(async (req, res) => {
   });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id);
+
+  if (!tour) {
+    return next(new AppError(`No tour found with that ID`, 404));
+  }
 
   res.status(200).json({
     status: 'success',
@@ -48,11 +53,15 @@ exports.createTour = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.updateTour = catchAsync(async (req, res) => {
+exports.updateTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
     runValidators: true,
   });
+
+  if (!tour) {
+    return next(new AppError(`No tour found with that ID`, 404));
+  }
 
   res.status(200).json({
     status: 'success',
@@ -62,8 +71,12 @@ exports.updateTour = catchAsync(async (req, res) => {
   });
 });
 
-exports.deleteTour = catchAsync(async (req, res) => {
-  await Tour.findByIdAndDelete(req.params.id);
+exports.deleteTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findByIdAndDelete(req.params.id);
+
+  if (!tour) {
+    return next(new AppError(`No tour found with that ID`, 404));
+  }
 
   res.status(204).json({
     status: 'success',
