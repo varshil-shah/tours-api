6ce41e42dd01db1ed650bff9cdab1ed9020f444d
Varshil-Shah <varshilashah09@gmail.com>
2022-07-21 12:25:44 +0530

    :sparkles: Including files in pug template

--- app.js
+++ app.js
@@ -70,7 +70,10 @@ app.use((req, res, next) => {
 
 // ROUTES
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'The Forest Hikers',
+    user: 'Varshil Shah',
+  });
 });
 
 app.use('/api/v1/tours', tourRouter);
