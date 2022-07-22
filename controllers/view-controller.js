2dbf921c1225f5553597adbf8f98e7fd77665b51
Varshil-Shah <varshilashah09@gmail.com>
2022-07-22 10:50:37 +0530

    :sparkles: Modified overview and add tour page

--- controllers/view-controller.js
+++ controllers/view-controller.js
@@ -1,11 +1,30 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tour-model');
+const catchAsync = require('../utils/catch-async');
+const AppError = require('../utils/app-error');
+
+exports.getOverview = catchAsync(async (req, res) => {
+  const tours = await Tour.find({});
+
   res.status(200).render('overview', {
     title: 'All Tours',
+    tours,
   });
-};
+});
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
+  const { slug } = req.params;
+
+  const tour = await Tour.findOne({ slug: slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+
+  if (!tour) {
+    next(AppError('No tour found with this name', 400));
+  }
+
   res.status(200).render('tour', {
     title: 'The Forest Hikers Tour',
+    tour,
   });
-};
+});
--- routes/view-router.js
+++ routes/view-router.js
@@ -3,6 +3,6 @@ const viewController = require('../controllers/view-controller');
 
 router.get('/', viewController.getOverview);
 
-router.get('/tour', viewController.getTour);
+router.get('/tour/:slug', viewController.getTour);
 
 module.exports = router;
