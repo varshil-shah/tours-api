5b47aa4e47bfd64385171555dafdb37af2726812
Varshil-Shah <varshilashah09@gmail.com>
2022-09-14 16:30:15 +0530

    :sparkles: Implement a route to check if user is logged in

--- routes/view-router.js
+++ routes/view-router.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
 const viewController = require('../controllers/view-controller');
+const authController = require('../controllers/auth-controller');
+
+router.use(authController.isLoggedIn);
 
 router.get('/', viewController.getOverview);
 router.get('/tour/:slug', viewController.getTour);
