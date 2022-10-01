7a2c2acb51fec418eaf357cf4b668892fae6e7a3
Varshil-Shah <varshilashah09@gmail.com>
2022-10-01 17:43:54 +0530

    :sparkles: Create new booking on checkout

--- controllers/booking-controller.js
+++ controllers/booking-controller.js
@@ -2,8 +2,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const Tour = require('../models/tour-model');
 const catchAsync = require('../utils/catch-async');
-const factory = require('./factory-controller');
-const AppError = require('../utils/app-error');
+const Booking = require('../models/booking-model');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // Get the currently booked tour
@@ -12,7 +11,9 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // Create checkout session
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    success_url: `${req.protocol}://${req.get('host')}/`,
+    success_url: `${req.protocol}://${req.get('host')}/?tour=${
+      req.params.tourId
+    }&user=${req.user.id}&price=${tour.price}`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
@@ -37,4 +38,12 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     status: 'success',
     session,
   });
+});
+
+exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+  const { tour, user, price } = req.query;
+  if (!tour && !user && !price) return next();
+
+  await Booking.create({ tour, user, price });
+  res.redirect(req.originalUrl.split('?')[0]);
 });
--- routes/view-router.js
+++ routes/view-router.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
 const viewController = require('../controllers/view-controller');
 const authController = require('../controllers/auth-controller');
+const bookingController = require('../controllers/booking-controller');
 
-router.get('/', authController.isLoggedIn, viewController.getOverview);
+router.get(
+  '/',
+  authController.isLoggedIn,
+  bookingController.createBookingCheckout,
+  viewController.getOverview
+);
 router.get('/tour/:slug', authController.isLoggedIn, viewController.getTour);
 router.get('/login', authController.isLoggedIn, viewController.getLoginPage);
 router.get('/me', authController.protect, viewController.getAccount);
