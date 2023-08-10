58284e3ad1e4c7224b1f5c13b76d3d0063eec607
Varshil-Shah <varshilashah09@gmail.com>
2023-08-11 00:18:20 +0530

    Set api-keys for image-kit

---
+++ utils/image-kit.js
@@ -0,0 +1,10 @@
+// SDK initialization
+const ImageKit = require('imagekit');
+
+const imagekit = new ImageKit({
+  publicKey: process.env.IMAGE_KIT_PUBLIC_KEY,
+  privateKey: process.env.IMAGE_KIT_PRIVATE_KEY,
+  urlEndpoint: process.env.IMAGE_KIT_URL_ENDPOINT,
+});
+
+module.exports = imagekit;
