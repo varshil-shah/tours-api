9107facb403cacf46e7a92cfedc7bd2fe6767a56
Varshil-Shah <varshilashah09@gmail.com>
2023-08-14 16:05:11 +0530

    Implement get and set for cache

--- controllers/factory-controller.js
+++ controllers/factory-controller.js
@@ -1,16 +1,37 @@
+const url = require('url');
+
 const catchAsync = require('../utils/catch-async');
 const AppError = require('../utils/app-error');
 const APIFeatures = require('../utils/api-features');
+const redisClient = require('../utils/redis-cache');
 
 exports.getOne = (Model, populateOptions) =>
   catchAsync(async (req, res, next) => {
     let query = Model.findById(req.params.id);
     if (populateOptions) query = query.populate(populateOptions);
 
+    const data = await redisClient.get(req.params.id);
+
+    if (data) {
+      return res.status(200).json({
+        status: 'success',
+        data: {
+          data: JSON.parse(data),
+        },
+      });
+    }
+
     const document = await query;
     if (!document) {
       return next(new AppError('No document found with that Id', 404));
     }
+
+    redisClient.set(
+      req.params.id,
+      JSON.stringify(document),
+      'EX',
+      process.env.REDIS_CACHE_EXPIRE
+    );
 
     res.status(200).json({
       status: 'success',
@@ -25,12 +46,35 @@ exports.getAll = (Model) =>
     let filter = {};
     if (req.params.tourId) filter = { tour: req.params.tourId };
 
+    if (!Object.keys(filter).length) {
+      const data = await redisClient.get(JSON.stringify(req.originalUrl));
+
+      if (data) {
+        const finalData = JSON.parse(data);
+
+        return res.status(200).json({
+          status: 'success',
+          results: finalData.length,
+          data: {
+            data: finalData,
+          },
+        });
+      }
+    }
+
     const features = new APIFeatures(Model.find(filter), req.query)
       .filter()
       .sort()
       .fieldLimit()
       .pagination();
     const document = await features.query;
+
+    redisClient.set(
+      JSON.stringify(req.originalUrl),
+      JSON.stringify(document),
+      'EX',
+      process.env.REDIS_CACHE_EXPIRE
+    );
 
     res.status(200).json({
       status: 'success',
